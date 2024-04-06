bd2224f15bbbef553434bb2e0ad480e81e10e133
Danil <danilkaspirin@gmail.com>
2024-04-06 11:24:07 +1000

    Init mte-gateway

---
+++ mte-gateway/src/main/java/ru/danilkaspirin/mtegateway/MteGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.danilkaspirin.mtegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MteGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MteGatewayApplication.class, args);
+    }
+
+}
